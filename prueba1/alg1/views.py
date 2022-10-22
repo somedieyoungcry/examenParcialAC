b162725f6b49e3ded83b5cab96432bdfdcf04be3
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2022-10-22 13:59:56 -0500

    Add Algoritmo KNN

--- prueba1/alg1/views.py
+++ prueba1/alg1/views.py
@@ -1,15 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
-from .models import post1
-# Create your views here.
+from p1.models import post1
+import math
 
-def muestra_datos(request):
-    consulta = post1.objects.all()
-    LisSum = suma(consulta)
-    contexto = zip(consulta, LisSum)
+def algKNN(request): 
+    x1 = 10
+    x2 = 20
+    x3 = 30
+    df = post1.objects.all()
+    ldis = distanciaEu(df,x1,x2,x3)
+    ldis = sorted(ldis)
+    contexto = zip(df,ldis)
     return render(request, 'alg1/algoritmo1.html', {'contexto':contexto})
 
-def suma(val):
-    listSum = []
-    for i in val:
-        listSum.append(i.x1 + i.x3 + i.x4)
-    return listSum
+def distanciaEu(df,x1,x2,x3):
+    ldist = []
+    for i in df:
+        dis = (x1 - i.x1)**2+(x2 - i.x3)**2+(x3 - i.x4)**2
+        raiz = round(math.sqrt(dis),4)
+        ldist.append(raiz)
+    return ldist
