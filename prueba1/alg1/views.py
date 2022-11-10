7f064fb85a6087a4a89c88d1ea64e928e6f6104f
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2022-11-09 23:20:05 -0600

    end knn

--- prueba1/alg1/views.py
+++ prueba1/alg1/views.py
@@ -1,28 +1,3 @@
-"""from multiprocessing import context
-from django.shortcuts import render
-from p1.models import post1
-#from alg1.templates.alg1 import *
-import math
-import random
-
-def algKNN(request): 
-    x1 = random.randint(1,500)
-    x2 = random.randint(1,500)
-    x3 = random.randint(1,500)
-    df = post1.objects.all()
-    ldis = distanciaEu(df,x1,x2,x3)
-    ldis = sorted(ldis)
-    contexto = zip(df,ldis)
-    return render(request, 'alg1/algoritmo1.html', {'contexto':contexto})
-
-def distanciaEu(df,x1,x2,x3):
-    ldist = []
-    for i in df:
-        dis = (x1 - i.x1)**2+(x2 - i.x3)**2+(x3 - i.x4)**2
-        raiz = round(math.sqrt(dis),4)
-        ldist.append(raiz)
-    return ldist"""
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from p1.models import post1
@@ -30,7 +5,7 @@ import math
 
 # Create your views here.
 def algKNN_list(request):
-    return render(request, 'alg1/algKNN.html')
+    return render(request, 'alg1/alg1.html')
 
 def buscar(request):
     if request.GET["k"].isdigit() and request.GET["x1"].isdigit() and request.GET["x2"].isdigit() and request.GET["x3"].isdigit():
@@ -46,9 +21,9 @@ def buscar(request):
         contado = contar(contexto, k)
         referencia = refe(contexto, contado, k)
         mensaje = referencia
-        return render(request, 'alg1/resultado.html', {'resultado': referencia})
+        return render(request, 'alg1/rKNN.html', {'resultado': referencia})
     else:
-        mensaje = "Te falto llenar o llenaste incorrectamente, recuerda que deben ser valores numericos"
+        mensaje = "Error en los datos"
     return HttpResponse(mensaje)
 
 def calculaDist(lista, x1, x2, x3):
@@ -88,7 +63,7 @@ def refe(contexto, contado, k):
     maxi = 0 
     variable = ''
     for equiv, i in zip(contexto, contado):
-        if i > maxi and cont < k: #el primero en superar
+        if i > maxi and cont < k:
             maxi = i
             variable = equiv[1]
         elif cont == k-1:
