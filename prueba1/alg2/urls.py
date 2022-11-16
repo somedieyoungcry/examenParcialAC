86dd3f3081e8c64dd47b7abd4e24833eed88b68a
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2022-11-15 22:24:03 -0600

    bayes naive not end

--- prueba1/alg2/urls.py
+++ prueba1/alg2/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('alg2/', views.muestra_datos, name='inicio'),
+    path('alg2/', views.algBayIng, name='inicio'),
 ]
--- prueba1/alg2/views.py
+++ prueba1/alg2/views.py
@@ -1,15 +1,85 @@
 from django.shortcuts import render
-from .models import post1
-# Create your views here.
+from p1.models import post1
+from django.http import HttpResponse
+import math 
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
-def muestra_datos(request):
-    consulta = post1.objects.all()
-    LisSum = suma(consulta)
-    contexto = zip(consulta, LisSum)
-    return render(request, 'alg2/algoritmo2.html', {'contexto':contexto})
+def algBayIng(request):
+    return render(request, 'alg2/algoritmo2.html')
 
-def suma(val):
-    listSum = []
-    for i in val:
-        listSum.append(i.x1 + i.x3 + i.x4)
-    return listSum
+def classBaye(request):
+    if request.GET["x1"].isdigit() and request.GET["x2"].isdigit() and request.GET["x3"].isdigit():
+        x1 = int(request.GET["x1"])
+        x2 = int(request.GET["x2"])
+        x3 = int(request.GET["x3"])
+        consulta = post1.objects.all()
+        media = media(request, consulta)
+        varianza = varianza(request, consulta)
+        equivalente = varEquivalente(consulta)
+        contexto = zip(media, varianza, equivalente)
+        contexto = sorted(contexto)
+        contado = contar(contexto, k)
+        referencia = refe(contexto, contado, k)
+        mensaje = referencia
+        return render(request, 'alg2/rBayeIng.html', {'resultado': referencia})
+    else:
+        mensaje = "Error en los datos"
+    return HttpResponse(mensaje)
+
+def media(request, lista):
+    lisMedia = []
+    for i in lista: 
+        media1 = np.mean(i.x1)
+        media2 = np.mean(i.x3)
+        media3 = np.mean(i.x4)
+        lisMedia.append(media1, media2, media3)
+    return lisMedia
+
+def varianza(request, lista):
+    lisVar = []
+    for i in lista:
+        var1 = np.var(i.x1)
+        var2 = np.var(i.x3)
+        var3 = np.var(i.x4)
+        lisVar.append(var1, var2, var3)
+    return lisVar
+
+def varEquivalente(consulta):
+    equivalente = []
+    for x in consulta:
+        equivalente.append(x.x2)
+    return equivalente
+
+def contar(contexto, k):
+    contadores = []
+    y = 0
+    for dist, variable in contexto:
+        if y < k:
+            z = 0
+            cont = 0
+            for dist, band in contexto:
+                if z < k:
+                    if variable == band:
+                        cont = cont + 1
+                else:
+                    break
+                z = z + 1
+            contadores.append(cont)
+        else:
+            break
+        y = y + 1
+    return contadores
+
+def refe(contexto, contado, k):
+    cont = 0
+    maxi = 0 
+    variable = ''
+    for equiv, i in zip(contexto, contado):
+        if i > maxi and cont < k:
+            maxi = i
+            variable = equiv[1]
+        elif cont == k-1:
+            break
+        cont = cont + 1
+    return variable
